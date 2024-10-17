e73fcb20d78d7d382d69ef5270bcdcb99241cb91
lukejagg <lukejagg@gmail.com>
2024-10-17 16:34:14 -0700

    Added paige script to child iframe

--- child/src/main.tsx
+++ child/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import executeInDevMode from '@speckai/paige';
+executeInDevMode("http://localhost:8080/npm/script.js");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
