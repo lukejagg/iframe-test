6a3e14763670fed0849802bb167d0cf18bb89b33
lukejagg <lukejagg@gmail.com>
2024-10-23 16:51:55 -0700

    Modified iframe to use sites deployment

--- parent/src/App.tsx
+++ parent/src/App.tsx
@@ -5,15 +5,27 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       // Verify the message origin
       // if (event.origin !== 'http://localhost:5174') return;
       console.log(`parent event.origin: ${event.origin}`);
-
-      // Handle the message
       console.log('Received message from iframe:', event.data);
+
+      const { type } = event.data;
+
+      if (type === "url") {
+        const { url } = event.data;
+        setUrl(url)
+      }
+
+      if (type === "add") {
+        const { componentHierarchy, changeMessage } = event.data;
+        console.log("componentHierarchy", componentHierarchy)
+        console.log("changeMessage", changeMessage)
+      }
 
       // Increment the counter
       setCount(prevCount => prevCount + 1);
@@ -33,11 +45,50 @@ function App() {
     iframe.contentWindow?.postMessage({ type: 'GREETING', text: 'Hello from parent' }, '*');
   };
 
+  const load = () => {
+    var iframe = document.getElementById('iframe-id') as HTMLIFrameElement;
+    var iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
+
+    if (!iframeDocument) {
+      console.error('iframeDocument is null');
+      return;
+    }
+
+    var scriptSource = `
+        window.addEventListener('DOMContentLoaded', function() {
+            var form = document.getElementById('form'); // change to the actual form selector
+            var input = document.getElementById('transaction_amount');
+            input.value = 100;
+
+            form.addEventListener('submit', function (ev) {
+                if (Number(input.value) > 100) {
+                    alert('No chance');
+                    ev.preventDefault();
+                }
+            });
+        });
+    `;
+    var script = iframeDocument.createElement("script");
+    var source = iframeDocument.createTextNode(scriptSource);
+    script.appendChild(source);
+    iframeDocument.body.appendChild(script);
+  }
+
+  const handleSelectComponent = () => {
+    var iframe = document.getElementById('iframe-id') as HTMLIFrameElement;
+    iframe?.contentWindow?.postMessage(
+      { type: "select_component", enabled: true },
+      "*",
+    );
+  };
+
   return (
     <>
-      <iframe id="iframe-id" src="http://localhost:5174" />
+      <iframe id="iframe-id" src="http://localhost:8081" onLoad={load} />
       <button onClick={sendMessageToIframe}>Send Message to Iframe</button>
+      <button onClick={handleSelectComponent}>Select Component</button>
       <p>Message count: {count}</p> {/* Display the counter */}
+      <p>URL: {url}</p>
     </>
   )
 }
